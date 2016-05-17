907bdd2a980d265a7aca853af422db4a74af8d75
Derekmod <derekmod@gmail.com>
2016-05-16 23:32:12 -0400

    Added some comments

--- AI_Battles/src/core/Move.java
+++ AI_Battles/src/core/Move.java
@@ -1,5 +1,14 @@
 package core;
 
+/**
+ * Abstract class for housing the information sent
+ * from Players to Games.
+ * 
+ * Awkwardly empty and plausibly permanently so.
+ * 
+ * @author Derek
+ *
+ */
 public abstract class Move {
 
 }
